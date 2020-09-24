53a056aa4c9184a4240230f709f64a98c7a4885e
Adrian Hall <photoadrian@outlook.com>
2020-09-24 08:12:03 -0700

    Backed out UAP compilation

--- src/Microsoft.Azure.Mobile.Client.SQLiteStore/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.Mobile.Client.SQLiteStore/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Resources;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
-[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: InternalsVisibleTo("Microsoft.WindowsAzure.MobileServices.SQLiteStore.Test")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
