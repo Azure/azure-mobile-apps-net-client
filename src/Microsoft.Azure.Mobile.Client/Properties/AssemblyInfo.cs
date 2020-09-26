62cdb14f0f3722b81b45cbf75e88f5b290c616fa
Adrian Hall <photoadrian@outlook.com>
2020-09-25 17:03:12 -0700

    UWP Test Suite

--- src/Microsoft.Azure.Mobile.Client/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.Mobile.Client/Properties/AssemblyInfo.cs
@@ -2,4 +2,7 @@
 
 [assembly: InternalsVisibleTo("Microsoft.WindowsAzure.MobileServices.Test")]
 [assembly: InternalsVisibleTo("Microsoft.WindowsAzure.MobileServices.SQLiteStore.Test")]
+[assembly: InternalsVisibleTo("Android.E2ETest")]
+[assembly: InternalsVisibleTo("iOS.E2ETest")]
+[assembly: InternalsVisibleTo("UWP.E2ETest")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
---
+++ test/UWP.E2ETest/UI/GroupDescription.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System.Collections.ObjectModel;
+
+namespace Microsoft.WindowsAzure.MobileServices.Test
+{
+    /// <summary>
+    /// UI model for a test group.
+    /// </summary>
+    class GroupDescription : ObservableCollection<TestDescription>
+    {
+        public string Name { get; set; }
+
+        public GroupDescription()
+        {
+        }
+    }
+}
