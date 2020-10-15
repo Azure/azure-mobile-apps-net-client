3904f4b903bcbb1b075915ef05745fe3a7a66cfd
Adrian Hall <adhal@microsoft.com>
2020-10-15 14:26:51 -0700

    Converted main client unit tests to xUnit

--- src/Microsoft.Azure.Mobile.Client/Properties/AssemblyInfo.cs
+++ src/Microsoft.Azure.Mobile.Client/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Microsoft.WindowsAzure.MobileServices.Test")]
 [assembly: InternalsVisibleTo("Microsoft.WindowsAzure.MobileServices.SQLiteStore.Test")]
 [assembly: InternalsVisibleTo("Android.E2ETest")]
 [assembly: InternalsVisibleTo("iOS.E2ETest")]
 [assembly: InternalsVisibleTo("UWP.E2ETest")]
+[assembly: InternalsVisibleTo("MobileClient.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
